4fae747e6b5f40bb2a9208f1ed87c2fa804a7c61
Clayman <Clayman@DESKTOP-DM8E77U>
2021-11-25 16:24:41 +0200

    String in progress.

---
+++ IntroductionToOOP/String/main.cpp
@@ -0,0 +1,140 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;;
+
+class String
+{
+	int size;	//Размер строки в Байтах
+	char* str;	//Указатель на строку в динамической памяти
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+
+	//				Constrcutors
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefConstruct:\t" << this << endl;
+	}
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
+		cout << "Constructor:\t" << this << endl;
+	}
+	String(const String& other)
+	{
+		//Deep copy:
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConstructor:" << this << endl;
+	}
+	~String()
+	{
+		delete[] this->str;
+		this->str = nullptr;
+		this->size = 0;
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//				Operators:
+	String& operator=(const String& other)
+	{
+		if (this == &other)return *this;
+		delete[] this->str;
+		//Deep copy:
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+
+	const char& operator[](int i)const
+	{
+		return str[i];
+	}
+	char& operator[](int i)
+	{
+		return str[i];
+	}
+
+	//				Methods:
+	void print()const
+	{
+		cout << "Size:\t" << size << endl;
+		cout << "Str:\t" << str << endl;
+	}
+};
+
+String operator+(const String& left, const String& right)
+{
+	String result(left.get_size() + right.get_size() - 1);
+	for (int i = 0; i < left.get_size(); i++)
+		//result.get_str()[i] = left.get_str()[i];
+		result[i] = left[i];
+	for (int i = 0; i < right.get_size(); i++)
+		//result.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+		result[i + left.get_size() - 1] = right[i];
+	return result;
+}
+
+ostream& operator<<(ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+//#define CONSTRUCTORS_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef CONSTRUCTORS_CHECK
+	String str(5);	//Default constrcutor
+	str.print();
+
+	String str1 = "Hello";
+	str1 = str1;
+	//str1.print();
+	cout << str1 << endl;
+
+	String str2 = str1;		//CopyConstructor
+	cout << str2 << endl;
+	//Deep copy
+	//Shallow copy
+
+	String str3;
+	str3 = str2;			//CopyAssignment (operator=)
+	str3.operator=(str2);
+	cout << str3 << endl;
+
+	int a = 2;
+	int b = 3;
+	a = b;
+	cout << a << endl;
+#endif // CONSTRUCTORS_CHECK
+
+	String str1 = "Hello";
+	String str2 = "World";
+	String str3 = str1 + str2;	//Неявно вызываем оператор +
+	cout << str3 << endl;
+	cout << operator+(str1, str2) << endl;	//Явный вызов оператора +
+	str1 += str2;
+	cout << str1 << endl;
+}
