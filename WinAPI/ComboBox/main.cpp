66f9de0956b83137b09c2184d9d76eb698b1a8d5
Clayman <Clayman@DESKTOP-DM8E77U>
2022-02-11 16:30:06 +0200

    MainWindow with metrics DONE

--- WinAPI/ComboBox/main.cpp
+++ WinAPI/ComboBox/main.cpp
@@ -11,7 +11,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, (DLGPROC)DlgProc, 0);
 	return 0;
 }
 
--- WinAPI/ReadMe.txt
+++ WinAPI/ReadMe.txt
@@ -1,3 +1,8 @@
-TODO:
+
+Cursors:http://www.rw-designer.com/cursor-set/metal-1
+
+DOCS:https://docs.microsoft.com/en-us/windows/win32/winmsg/windowing
+
+DONE:
 Добавить проект ListBox, и создать такое же окно как с ComboBox, только с ListBox.
 https://docs.microsoft.com/en-us/windows/win32/controls/list-boxes
